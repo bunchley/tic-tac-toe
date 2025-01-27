0d8c382af1fe2de1f0d1ea4bc7af3119f0832106
Ashley Bunch <bunchley007@gmail.com>
2025-01-26 21:31:43 -0700

    winnig move check in place

--- src/game.js
+++ src/game.js
@@ -5,10 +5,8 @@ let Player = function Player(name, marker) {
   this.name = name;
   this.marker = marker;
 };
+const wins = [123, 456, 789, 147, 258, 369, 159, 357];
 
-Player.prototype.displayPlayer = function () {
-  console.log("display player");
-};
 const board = document.querySelector(".board");
 
 const playerOne = new Player("Ashley", "X");
@@ -25,6 +23,10 @@ const boardDisplay = {
   eight: "",
   nine: "",
 };
+
+function checkAgainstWinningMoves() {
+  console.log("Checking against winning Moves");
+}
 function showCurrentGameBoard() {
   for (spot in boardDisplay) {
     let placement = document.getElementById(`${spot}`);
@@ -32,24 +34,50 @@ function showCurrentGameBoard() {
   }
 }
 
-function displayCurrentPlayerName(player) {
+function displayCurrentPlayerName(player, title) {
   let playerName = `${player.marker}:${player.name}'s Turn`;
+  if (title === true) {
+    playerName = `${player.marker}:${player.name}`;
+    document.querySelector(".user").textContent = `${playerOne.name}`;
+    document.querySelector(".robot").textContent = `${playerTwo.name}`;
+  }
   playerTurnDisplay.textContent = `${playerName}`;
 }
-function gameCheck() {
+function gameCheck(player) {
   console.log("Game checked for a winner");
-  winCheck = false;
-  if (!winCheck) {
-  } else {
-    console.log("winner winner");
-  }
-}
+  let playerArray = [];
+  let position = 1;
 
+  for (property in boardDisplay) {
+    if (boardDisplay[property] === player.marker) {
+      playerArray += [position];
+    }
+    position += 1;
+  }
+  for (i = 0; i < wins.length; i++) {
+    if (playerArray.length > 2) {
+      let winner = playerArray.includes(`${wins[i]}`);
+      if (winner === true) {
+        console.log(`And the winner is...`, ` ${player.marker}`);
+        return winner;
+      }
+      console.log("here is the win combo", wins[i]);
+      console.log("Issssss there a winner?", winner);
+    }
+  }
+
+  position = 0;
+  console.log(playerArray);
+}
+function gameOver(player) {
+  console.log(`player wins`, player.name);
+  // clear board
+}
 function playGame() {
   //   displayPlayerOneName();
   showCurrentGameBoard();
 
-  displayCurrentPlayerName(playerOne);
+  displayCurrentPlayerName(playerOne, true);
   //   let currentplayer = `${playerOne.marker}`;
   board.addEventListener("click", (event) => {
     const isButton = event.target.nodeName === "BUTTON";
@@ -62,16 +90,18 @@ function playGame() {
       boardDisplay[`${position}`] = `${currentplayer}`;
       console.log(boardDisplay);
 
-      gameCheck();
+      if (gameCheck(playerOne) === true) {
+        gameOver(playerOne);
+      }
       currentplayer = "O";
-      displayCurrentPlayerName(playerTwo);
+      displayCurrentPlayerName(playerTwo, false);
     } else if (currentplayer === "O") {
       position = event.target.id;
       boardDisplay[`${position}`] = `${currentplayer}`;
       console.log(boardDisplay);
-      gameCheck();
+      gameCheck(playerTwo);
       currentplayer = "X";
-      displayCurrentPlayerName(playerOne);
+      displayCurrentPlayerName(playerOne, false);
     }
     showCurrentGameBoard();
   });
